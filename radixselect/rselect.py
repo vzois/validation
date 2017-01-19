2419a5e6d0e3a4f8994a5fdff14db600a2705137
Vasileios Zois <vzois@usc.edu>
2017-01-18 18:17:12 -0800

    fixed radixselect

--- radixselect/rselect.py
+++ radixselect/rselect.py
@@ -3,7 +3,7 @@ from random import randint
 from heapq import nsmallest
 
 
-MAX_VALUE = 1024
+MAX_VALUE = 1024*1024
 
 def hexDigit(x,d):
     shf = d << 2
@@ -17,45 +17,52 @@ def setHex(x,d,v):
 
 def swith(x,p,m):
     return ((x & m) == p )
-    
 
 def radixselect(input,k):
-    max_value = 0xFFFFFFFF
-    mask = 0x0
-    pred = 0x0
-    
-    buckets = [0 for i in range(16)]
+    buckets = [0 for i in range(17)]
     tmpK = k
-    
+    num = 0x0
+    mask = 0x0
+
+    debug = False
     for d in range(7,-1,-1):
-        buckets = [0 for i in range(16)]
+        buckets = [0 for i in range(17)]
         for i in input:
-            if swith(i,pred,mask):         
+            #if i <= num:
+            if swith(i,num,mask):
                 digit = hexDigit(i,d)
-                buckets[digit]+=1
-
-        print "<1>",d,buckets
-        
-        digit = 0
-        for i in range(0,15):
+                buckets[digit+1]+=1
+        if debug:
+            print "<1>",d,buckets
+        for i in range(1,16):
             buckets[i+1] += buckets[i]
-            
-        for i in range(0,15):
-            if buckets[i] >= tmpK:
-                digit=i
+        if debug:
+            print "<2>",d,buckets
+        
+        pos = 0
+        for i in range(1,17):
+            if tmpK <= buckets[i]:
+                pos = i
                 break
+        if debug:
+            print "digit:",hex(pos),hex((pos-1))
+            print "tmpK:",tmpK,"buckets:",buckets[i-1]
+            
+        if tmpK != buckets[i-1]:
+            tmpK = tmpK - buckets[i-1]
+        if debug:
+            print "{",tmpK,"}"
         
-        tmpK = tmpK - buckets[i-1]
-        print tmpK
-        #pred = setHex(pred,d,digit)
-        #print "<2>{",d,"}(",hex(digit),")",hex(pred),buckets
-        mask = mask | ( 0xF << (d << 2))
-        
-        #print "---------------------------------------------------"
+        num = setHex(num,d,(pos-1))
+        if debug:
+            print "num:",hex(num)
+        mask = mask | (0xF << (d << 2))
+        if debug:
+            print "m:",hex(mask)
+            print "---------------------------------------------------------------------"
 
-    #print buckets
-    print "k-th largest",max_value,hex(max_value)
-
+    print "(rd) k-smallest element:", num,hex(num)
+    
 if len(sys.argv) < 3:
     print "Please provide a valid vector size and k-th element value!!!"
     exit(1)
@@ -70,22 +77,13 @@ print "Size:", size
 data = [randint(0,MAX_VALUE) for i in range(size)]
 #print data
 
-for d in data:
-    print hex(d),hex(hexDigit(d,3))
+#for d in data:
+#    print hex(d),hex(hexDigit(d,3))
 
 nsmall = nsmallest(k,data)
 kmax=max(nsmall)
-print "k smallest elements:", nsmall,kmax, hex(kmax)
-#print "k smallest element:", kmax,hex(kmax)
 
 radixselect(data,k)
 
-
-
-
-
-
-
-
-
-
+#print "k smallest elements:", nsmall,kmax, hex(kmax)
+print "(py) k smallest element:", kmax,hex(kmax)
